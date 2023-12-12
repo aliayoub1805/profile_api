1ba20feedf06df9c9cca963bc46c889bbfdfe0fa
Ali Home <aliayoub_1987@windowslive.com>
2023-12-12 21:47:59 +0200

    Added Viewset hello-viewset

--- profile_api_app/views.py
+++ profile_api_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
+from rest_framework import viewsets
 from profile_api_app import serializers
 
 class HelloApiView(APIView):
@@ -48,3 +48,18 @@ class HelloApiView(APIView):
         """Delete an object"""
 
         return Response({'method': 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+
+    def list(self, request):
+        """Return a hello message."""
+
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLS using Routers',
+            'Provides more functionality with less code',
+        ]
+
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
