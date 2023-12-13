24e9c282ab77290bad27fe1026759030b82c8d41
Ali Home <aliayoub_1987@windowslive.com>
2023-12-13 12:39:57 +0200

    Added seraching to user profile api

--- profile_api_app/views.py
+++ profile_api_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, viewsets
+from rest_framework import status, viewsets, filters
 from profile_api_app import serializers, models, permissions
 from rest_framework.authentication import TokenAuthentication
 
@@ -107,3 +107,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
