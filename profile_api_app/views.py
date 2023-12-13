a048b96c12cde0d476d3d691a50c8947be4ac54a
Ali Home <aliayoub_1987@windowslive.com>
2023-12-13 12:44:04 +0200

    Added user login to user profile api

--- profile_api_app/views.py
+++ profile_api_app/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from rest_framework import status, viewsets, filters
 from profile_api_app import serializers, models, permissions
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -109,3 +111,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+class UserLoginApiView(ObtainAuthToken):
+   """Handle creating user authentication tokens"""
+   renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
