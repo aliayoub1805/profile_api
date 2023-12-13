0401e59e7c581625655ca4c3795a4130297627c5
Ali Home <aliayoub_1987@windowslive.com>
2023-12-13 12:36:28 +0200

    Added viewset to profiles api and permission file

--- profile_api_app/views.py
+++ profile_api_app/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
-from rest_framework import viewsets
-from profile_api_app import serializers
+from rest_framework import status, viewsets
+from profile_api_app import serializers, models, permissions
+from rest_framework.authentication import TokenAuthentication
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -97,3 +97,13 @@ class HelloViewSet(viewsets.ViewSet):
         """Handle removing an object"""
 
         return Response({'http_method': 'DELETE'})
+
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating, creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
