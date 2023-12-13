7024ded7a994c0c9392be06ead9b1624bb761b8d
Ali Home <aliayoub_1987@windowslive.com>
2023-12-13 13:22:12 +0200

    Added profile feed item

--- profile_api_app/views.py
+++ profile_api_app/views.py
@@ -115,3 +115,23 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 class UserLoginApiView(ObtainAuthToken):
    """Handle creating user authentication tokens"""
    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+        #serializer.save(user_profile=self.request.user.userprofile)
+        if self.request.user.is_authenticated:
+            user_profile = self.request.user
+            serializer.save(user_profile=user_profile)
+        else:
+            # Handle the case where the user is not authenticated
+            # For example, you might choose to return a 401 Unauthorized response
+            return Response(
+                {"error": "Authentication required"},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
